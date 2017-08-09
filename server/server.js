d83beee947becfc19b5d0041d585edac77a8bf9d
Dinesh <dineshsirvi01@gmail.com>
2017-08-09 19:03:55 +0530

    Adding socket and handling events

--- server/server.js
+++ server/server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../public')));
 
+io.on('connection' ,(socket)=>{
+  console.log("A new user is connected");
+
+  socket.on('createMessage', (newMsg)=>{
+    console.log(newMsg);
+  });
+
+  socket.emit('newMessage', {
+    "sender": "someOne",
+    "text": "Hello there!"
+  });
+
+  socket.on('disconnect', ()=>{
+    console.log("A user has beed disconnected");
+  });
+
+});
 
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
   console.log(`Server is up on PORT ${port}`);
 });
